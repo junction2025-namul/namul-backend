8a5ae512539316acf383866adf8e970038d36c71
Leesumok <leesumok_@naver.com>
2025-08-23 14:38:08 +0900

    add : swagger 접근 재설정

--- src/main/kotlin/com/junction/namul/config/SwaggerConfig.kt
+++ src/main/kotlin/com/junction/namul/config/SwaggerConfig.kt
@@ -5,21 +5,12 @@ import io.swagger.v3.oas.models.info.Info
 import io.swagger.v3.oas.models.servers.Server
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
-import org.springframework.core.env.Environment
 
 @Configuration
-class SwaggerConfig(
-    private val environment: Environment
-) {
+class SwaggerConfig {
     
     @Bean
     fun openAPI(): OpenAPI {
-        val servers = if (environment.activeProfiles.contains("local")) {
-            listOf(Server().url("http://localhost:8080").description("Local server"))
-        } else {
-            listOf(Server().url("https://namul-backend-production-e240.up.railway.app").description("Production server"))
-        }
-        
         return OpenAPI()
             .info(
                 Info()
@@ -27,6 +18,10 @@ class SwaggerConfig(
                     .description("Namul API Documentation")
                     .version("1.0.0")
             )
-            .servers(servers)
+            .servers(
+                listOf(
+                    Server().url("https://namul-backend-production-e240.up.railway.app")
+                )
+            )
     }
 }
